7fade688a8a9df70507d528cf30da1eb83384cfc
NikoManiac <niko.sky.lan@gmail.com>
2019-03-03 16:43:27 +0800

    base process for install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 INSTALL_FILE_NAME="install.sh"
 DVM_HOME="$HOME/.dvm"
-
+DVM_COMMAND_REPO="https://github.com/mangostano/dvm/blob/master/dvm"
 
 has_command() {
   if ! command -v "$1" > /dev/null 2>&1
@@ -15,15 +15,16 @@ get_installed_dotnet(){
     echo dotnet --version
 }
 
-command_installed_path(){
-  echo which $1
+installed_dotnet_path(){
+    INSTALLED_DOTNET_PATH=$(command -v dotnet)
+    echo "${INSTALLED_DOTNET_PATH%dotnet}"
 }
 
 # check the curl or wget command
 if [[ 0 -eq $(has_command curl) ]]; then
-     DVM_INATSLL_COMMAND="curl"
+     DVM_INSTALL_COMMAND="curl"
 elif [ 0 -eq $(has_command wget) ]; then
-     DVM_INATSLL_COMMAND="wget"
+     DVM_INSTALL_COMMAND="wget"
   else
      echo "[ERROR] please install `wget` or `curl` command first"
      exit 1
@@ -35,26 +36,28 @@ echo "START INSTALL DVM COMMAND"
 mkdir -p ${DVM_HOME}/sdks
 mkdir -p ${DVM_HOME}/scripts
 
-if [[ ! -f "$DMV_HOME/$INSTALL_FILE_NAME" ]]; then
-  rm -rf $INSTALL_FILE_NAME
+if [[ ! -f "$DMV_HOME/scripts/$INSTALL_FILE_NAME" ]]; then
+  rm -f "$DMV_HOME/scripts/$INSTALL_FILE_NAME"
 fi
 
 # user had installed dotnet by other way
-if [[ 1 -eq $(has_command dotnet) ]]; then
+if [[ 0 -eq $(has_command dotnet) ]]; then
     echo "Your had installed the dotnet by other ways, dvm is handle.\n After this, you can continue use the current version"
     sudo rm /etc/paths.d/dotnet
-    CURRENT_DOTNET_VERSION = $(get_installed_dotnet)
-    mv -f $(command_installed_path dotnet)/sdk $DVM_HOME/sdks
+    CURRENT_DOTNET_VERSION=$(get_installed_dotnet)
+    # mv the installed sdk to dvm/SDKs
+    sudo mv -rf $(installed_dotnet_path)/sdk/* $DVM_HOME/sdks
 fi
 
+# curl the microsoft dotnet install script
 $DVM_INSTALL_COMMAND https://dot.net/v1/dotnet-install.sh > $DVM_HOME/scripts/$INSTALL_FILE_NAME
-
 chmod +X $DVM_HOME/scripts/$INSTALL_FILE_NAME
 
-echo "# This is for DVM command" >> 
+# curl the dvm command from repo && need user to update path
+$DVM_INSTALL_COMMAND $DVM_COMMAND_REPO > $DVM_HOME/dvm
 
-# get the current shell and remind the user to update the $PATH
-echo "Please update the to"
+echo "# This is for DVM command\n"
+echo "Please add the\n\n"
+echo "export PATH=\$HOME/.dvm:\$PATH\n\n"
+echo "to your PATH variable\n"
 echo "INSTALLATION COMPLETED, PLEASE ENJOY!"
-# bash $INSTALL_FILE_NAME -Channel 1.0
-# sudo ln -s $USERHOME/.dotnet/sdk/1.1.11 /usr/local/share/dotnet/sdk/
