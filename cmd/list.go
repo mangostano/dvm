a480e13bbbc5c106673843511a829bce4c43cc8e
XiaoQu <lqqu@thoughtworks.com>
2019-03-17 19:45:20 +0800

    finish list command

--- cmd/list.go
+++ cmd/list.go
@@ -1,38 +1,31 @@
-// Copyright © 2019 NAME HERE <EMAIL ADDRESS>
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package cmd
 
 import (
 	"fmt"
-
 	"github.com/spf13/cobra"
+	"log"
+	"os/exec"
 )
 
 // listCmd represents the list command
 var listCmd = &cobra.Command{
 	Use:   "list",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "list you local installed dotnet core sdk version",
+	Long: `examples of using this command. For example:
+    dvm list   -- list all local sdk`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("list called")
+		getLocalList()
 	},
+}
+
+func getLocalList() {
+	sdksPath := getDvmSdkStorePath("")
+	cmd := exec.Command("ls", sdksPath)
+	out, err := cmd.Output()
+	if err != nil {
+		log.Fatal("list installed sdk failed. ", contactUs)
+	}
+	fmt.Println(string(out))
 }
 
 func init() {
