e6412156ae28133a9a693721f258ad92fceeecac
XiaoQu <lqqu@thoughtworks.com>
2019-03-17 16:28:27 +0800

    finish listAll with sort and format (read from local json file)

--- cmd/listAll.go
+++ cmd/listAll.go
@@ -1,37 +1,43 @@
-// Copyright © 2019 NAME HERE <EMAIL ADDRESS>
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
-
 	"github.com/spf13/cobra"
+	"io/ioutil"
+	"log"
+	"os"
+	"sort"
 )
 
 // listAllCmd represents the listAll command
 var listAllCmd = &cobra.Command{
 	Use:   "listAll",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "This command to get all of the dotnet core sdk versions",
+	Long: `examples of using this command. For example:
+		dvm list-all`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("listAll called")
+		jsonFile, err := os.Open("/Users/lqqu/go/src/dotnet-manager/dvm/config/versions.json")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer jsonFile.Close()
+
+		byteValue, _ := ioutil.ReadAll(jsonFile)
+		result := make(map[string][]string)
+		json.Unmarshal([]byte(byteValue), &result)
+
+		var mainVersions []string
+		for mainVersion := range result {
+			mainVersions = append(mainVersions, mainVersion)
+		}
+		sort.Strings(mainVersions)
+		for _, mainVersion := range mainVersions {
+			fmt.Println("    ", mainVersion)
+			for index := range result[mainVersion] {
+				fmt.Println("\t", result[mainVersion][index])
+			}
+		}
 	},
 }
 
