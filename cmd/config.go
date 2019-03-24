1c51dcf26491de51337562e8f0492a5e3ddd3de9
XiaoQu <lqqu@thoughtworks.com>
2019-03-24 15:53:55 +0800

    fix version file url template error when using tag

--- cmd/config.go
+++ cmd/config.go
@@ -2,6 +2,6 @@ package cmd
 
 const currentVersion = "0.1.0"
 
-const versionFileUrlTemplate = "https://raw.githubusercontent.com/mangostano/dvm/tree/%s/config/versions.json"
+const versionFileUrlTemplate = "https://raw.githubusercontent.com/mangostano/dvm/%s/config/versions.json"
 
 const contactUs = "please contact us.(https://github.com/mangostano/dvm/issues)"
