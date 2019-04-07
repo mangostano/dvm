7d7fd60c3168d6e01a781ae26b23b4aba637066b
XiaoQu <lqqu@thoughtworks.com>
2019-04-07 14:57:11 +0800

    hide alias command

--- cmd/alias.go
+++ cmd/alias.go
@@ -36,7 +36,7 @@ to quickly create a Cobra application.`,
 }
 
 func init() {
-	rootCmd.AddCommand(aliasCmd)
+	// rootCmd.AddCommand(aliasCmd)
 
 	// Here you will define your flags and configuration settings.
 
